b90d24bc80715f8682575d7f43ca755de98e61d8
coding-practice <laibakhalid2109@gmail.com>
2025-09-18 04:34:26 +0500

    fix

--- src/components/addToCart/addToCart.jsx
+++ src/components/addToCart/addToCart.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useCartStore } from "../CartContext/cartContext";
 
-function AddToCartBtn({product,setmsg}) {
+function AddToCartBtn({ product, setmsg }) {
 
     const addToCart = useCartStore((state) => state.addToCart);
 
-    const handleClick = (productId) => {
+    const handleClick = (e, productId) => {
+        e.preventDefault();
+        e.stopPropagation();
         setmsg(`Product with id ${productId} has been added!`);
         setTimeout(() => setmsg(false), 3000);
     };
@@ -14,16 +16,16 @@ function AddToCartBtn({product,setmsg}) {
     };
     return (
         <>
-                <button
-                    className="bg-blue-950 h-10 w-full font-semibold transition duration-200 ease-in-out text-white rounded hover:bg-white hover:text-black border hover:border-black"
-                    onClick={() => {
-                        handleClick(product.id);
-                        handleAdd(product);
-                    }}
-                >
-                    Add to Cart
-                </button>
-       </>
+            <button
+                className="bg-blue-950 h-10 w-full font-semibold transition duration-200 ease-in-out text-white rounded hover:bg-white hover:text-black border hover:border-black"
+                onClick={(e) => {
+                    handleClick(e,product.id);
+                    handleAdd(product);
+                }}
+            >
+                Add to Cart
+            </button>
+        </>
     )
 }
 export default AddToCartBtn;
